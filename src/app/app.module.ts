a764e3a6662c1e541ff6f5d3453071dfc00b30df
lamstutz <louis.amstutz@gmail.com>
2018-05-08 17:07:30 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+
+
+import {MatInputModule} from '@angular/material/input';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/search-bar/search-bar.component.ts
@@ -0,0 +1,89 @@
+import { Component, OnInit } from '@angular/core';
+import * as electron from 'electron';
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.css']
+})
+export class SearchBarComponent implements OnInit {
+
+  constructor() { }
+
+//   ngOnInit() {
+  
+
+
+//   // Dans le processus renderer.
+
+  
+//   desktopCapturer.getSources({types: ['window', 'screen']}, (error, sources) => {
+//     if (error) throw error
+//     for (let i = 0; i < sources.length; ++i) {
+//       if (sources[i].name === 'Electron') {
+//         navigator.mediaDevices.getUserMedia({
+//           audio: false,
+//           video: {
+//             mandatory: {
+//               chromeMediaSource: 'desktop',
+//               chromeMediaSourceId: sources[i].id,
+//               minWidth: 1280,
+//               maxWidth: 1280,
+//               minHeight: 720,
+//               maxHeight: 720
+//             }
+//           }
+//         })
+//         .then((stream) => this.handleStream(stream))
+//         .catch((e) => this.handleError(e))
+//         return
+//       }
+//     }
+//   });
+// }
+  
+//    handleStream (stream) {
+//     const video = document.querySelector('video')
+//     video.srcObject = stream
+//     video.onloadedmetadata = (e) => video.play()
+//   }
+  
+//    handleError (e) {
+//     console.log(e)
+//   }
+
+ngOnInit() {
+  let n = <any>navigator;
+  let myvid = <HTMLInputElement>document.getElementById('raja');
+  electron.desktopCapturer.getSources({types: ['window', 'screen']}, (error, sources) => {
+  console.log(sources);
+  if (error) throw error;
+  for (let i = 0; i < sources.length; ++i) {
+  if (sources[i].name === 'YvonElectron') {
+  n.webkitGetUserMedia({
+  audio: false,
+  video: {
+  mandatory: {
+  chromeMediaSource: 'desktop',
+  chromeMediaSourceId: sources[i].id,
+  minWidth: 1280,
+  maxWidth: 1280,
+  minHeight: 720,
+  maxHeight: 720
+  }
+  }
+  }, gotStream, getUserMediaError);
+  return;
+  }
+  }
+ });
+  
+ function gotStream(stream) {
+  myvid.src = URL.createObjectURL(stream);
+ }
+  
+ function getUserMediaError(e) {
+  console.log('getUserMediaError');
+ }
+  }
+
+}
